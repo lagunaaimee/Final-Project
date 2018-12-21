1ecbba793708e777c7685f4c3ad5903eb620814d
RHR <renee.h.reynolds@gmail.com>
2018-12-20 18:46:08 -0700

    API routing/working

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
+const meetupRoutes = require("./meetups");
 
 // Book routes
-router.use("/books", bookRoutes);
+router.use("/meetups", meetupRoutes);
 
 module.exports = router;
---
+++ routes/api/meetups.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const meetupsController = require("../../controllers/meetupsController");
+
+// Matches with "/api/meetups"
+router.route("/")
+  .get(meetupsController.findAll)
+  .post(meetupsController.create);
+
+// Matches with "/api/meetups/:id"
+router
+  .route("/:id")
+  .get(meetupsController.findById)
+  .put(meetupsController.update)
+  .delete(meetupsController.remove);
+
+module.exports = router;
