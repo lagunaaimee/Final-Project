7dc1902de4ece679dcbd7ce00c46870b890cf6f6
Aimee <aimee.n.laguna@gmail.com>
2018-12-31 18:28:42 -0700

    Small updates to front-end

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => (
+    // <!-- Footer -->
+    <footer className="footer py-3 footer-expand-lg footer-dark bg-dark">
+      <div className="container">
+        <p className="m-0 text-center text-white">Copyright © DevHub</p>
+      </div>
+      {/* <!-- /.container --> */}
+    </footer>
+  );
+  
+  export default Footer;
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Nav = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div className="container">
-      <a className="navbar-brand" href="/">Our Specatular Title</a>
+      <a className="navbar-brand" href="/">Dev Hub</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -15,16 +15,16 @@ const Nav = () => (
             </a>
           </li>
           <li className="nav-item">
-            <a className="nave-link" href="/api/Meetup">Meetup</a>
+            <a className="nav-link" href="/api/Meetup">Meetup</a>
           </li>
           <li className="nav-item">
-            <a className="nave-link" href="/api/Forum">Forum </a>
+            <a className="nav-link" href="/api/Forum">Forum </a>
           </li>
           <li className="nav-item">
-            <a className="nave-link" href="/api/Blog">Blog</a>
+            <a className="nav-link" href="/api/Blog">Blog</a>
           </li>
           <li className="nav-item">
-            <a className="nave-link" href="/api/Resources">Resources</a>
+            <a className="nav-link" href="/api/Resources">Resources</a>
           </li>
         </ul>
       </div>
