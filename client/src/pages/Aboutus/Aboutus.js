80c607d6d63734ed284a1e643c7a59b0e29b9ec8
Aimee <aimee.n.laguna@gmail.com>
2019-01-17 21:37:49 -0700

    updated photos on each page, added article content on Resources page

--- client/src/pages/Aboutus/Aboutus.js
+++ client/src/pages/Aboutus/Aboutus.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 // import Jumbotron from "../../components/Jumbotron";
 import API from "../../utils/API";
 // import DeleteBtn from "../../components/DeleteBtn";
-// import { Col, Row, Container } from "../../components/Grid";
+import { Col, Row, Container } from "../../components/Grid";
 // import { List, ListItem } from "../../components/List";
 // import { Input, TextArea, FormBtn } from "../../components/Form";
 
@@ -23,28 +23,29 @@ class Aboutus extends Component {
 
   render() {
     return (
-      <div className="Container fluid">
+      <Container fluid>
+        <Col size="sm-12">
+          <img className="img-fluid rounded" src="https://slack-imgs.com/?c=1&url=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F1068523%2Fpexels-photo-1068523.jpeg%3Fauto%3Dcompress%26cs%3Dtinysrgb%26fit%3Dcrop%26h%3D627%26w%3D1200" alt="">
+          </img>
+        </Col>
+        <br></br>
         {/* <Row> */}
-        <div className="row align-items-center">
-          <div className="col-lg-12">
-            <img className="img-fluid rounded" src="https://images.pexels.com/photos/1509428/pexels-photo-1509428.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-            </img>
-          </div>
+        <Row>
           {/* <!-- /.col-lg-8 --> */}
           <div className="col-md-12 text-center">
             <h2>Purpose</h2>
             <p>
-The purpose of this application is to create a centralized location for post-coding-boot-camp graduates. The application will accomplish this purpose by providing graduates with an online means of scheduling face-to-face meetups (in-person or virtually), which will provide a sense of community, opportunity for skills advancement, and portfolio development.
+              The purpose of this application is to create a centralized location for post-coding-boot-camp graduates. The application will accomplish this purpose by providing graduates with an online means of scheduling face-to-face meetups (in-person or virtually), which will provide a sense of community, opportunity for skills advancement, and portfolio development.
 </p>
-<h2>Value</h2>
-<p>
-This application is intended for students who are in a transition phase between boot camp graduation to precuring their position as a web developer. Therefore, the value of this application is to bridge the gap between the rigors of boot camp and the realities of the professional world, most specifically addressing the areas of confidence that many graduates retain even after a thorough professional boot camp experience: having a strong professional network, building an impressive and professional portfolio of work, increasing programming language knowledge, and improving upon computer science understanding. 
- 
-Through the community building offered, this app helps to build skills and contribute to holistic professional development.
+            <h2>Value</h2>
+            <p>
+              This application is intended for students who are in a transition phase between boot camp graduation to precuring their position as a web developer. Therefore, the value of this application is to bridge the gap between the rigors of boot camp and the realities of the professional world, most specifically addressing the areas of confidence that many graduates retain even after a thorough professional boot camp experience: having a strong professional network, building an impressive and professional portfolio of work, increasing programming language knowledge, and improving upon computer science understanding.
+
+              Through the community building offered, this app helps to build skills and contribute to holistic professional development.
 </p>
-<h2>General Overview</h2>
-<p>
-The application will utilize the Meetup API to find already established groups in the area. Further, resource websites that promote skill development for full-stack devs will be utilized by scraping articles and centralizing them within the app’s resources tab. Finally, the app will utilize authentication. The app will have four main sections: Meetups, forums, blogs, and resources. The design is loosely based on The Grad Cafe, which provides potential graduate students with resources during their transition to graduate school.
+            <h2>General Overview</h2>
+            <p>
+              The application will utilize the Meetup API to find already established groups in the area. Further, resource websites that promote skill development for full-stack devs will be utilized by scraping articles and centralizing them within the app’s resources tab. Finally, the app will utilize authentication. The app will have four main sections: Meetups, forums, blogs, and resources. The design is loosely based on The Grad Cafe, which provides potential graduate students with resources during their transition to graduate school.
 </p>
           </div>
           <div className="card text-white bg-secondary my-4 col-md-12 text-center">
@@ -77,63 +78,12 @@ The application will utilize the Meetup API to find already established groups i
                   <a href="https://github.com/lagunaaimee"><h2>Aimee Laguna</h2></a>
                   <p>Project Role</p>
                 </div>
+                
               </div>
             </div>
           </div>
-
-          {/* Div displaying page descriptions*/}
-
-          <div className="row">
-            <div className="col-md-4 mb-4 text-center">
-              <div className="card h-100">
-                <div className="card-body">
-                  <a className="card-title" href="meetup">Meetup</a>
-                  <p className="card-text">Find local meets in the Tucson Area to network and sharpen your coding skills!</p>
-                </div>
-                <div className="card-footer">
-                  <a href="/meetup" class="btn btn-primary">Join</a>
-                </div>
-              </div>
-            </div>
-
-            {/* <div class="col-md-4 mb-4">
-              <div class="card h-100">
-                <div class="card-body">
-                  <a className="card-title" href="api/Forum">Forum</a>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod tenetur ex natus at dolorem enim! Nesciunt pariatur voluptatem sunt quam eaque, vel, non in id dolore voluptates quos eligendi labore.</p>
-                </div>
-                <div class="card-footer">
-                  <a href="#" class="btn btn-primary">More Info</a>
-                </div>
-              </div>
-            </div> */}
-
-            <div className="col-md-4 mb-4 text-center">
-              <div className="card h-100">
-                <div className="card-body">
-                  <a className="card-title" href="/blog">Blog</a>
-                  <p className="card-text">Read about current articles that your fellow colleagues are writing about!</p>
-                </div>
-                <div className="card-footer">
-                  <a href="/blog" class="btn btn-primary">Post</a>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-md-4 mb-4 text-center">
-              <div class="card h-100">
-                <div class="card-body">
-                  <a className="card-title" href="/resources">Resources</a>
-                  <p class="card-text">Here you'll find relevant resources about the coding community on Reddit!</p>
-                </div>
-                <div class="card-footer">
-                  <a href="/resources" class="btn btn-primary">Follow</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div >
-      </div >
+        </Row>
+      </Container>
     );
   }
 }
--- client/src/pages/Meetup/Meetup.js
+++ client/src/pages/Meetup/Meetup.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
-import Jumbotron from "../../components/Jumbotron";
 import API from "../../utils/API";
-import DeleteBtn from "../../components/DeleteBtn";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
-import { Input, TextArea, FormBtn } from "../../components/Form";
+
 
 class Meetup extends Component {
   // Setting our component's initial state
@@ -15,16 +13,17 @@ class Meetup extends Component {
     city: "",
     state: "",
     who: "",
+    description: "",
   };
 
-    // When the component mounts, load all meetups and save them to this.state.Meetup
+  // When the component mounts, load all meetups and save them to this.state.Meetup
   componentDidMount() {
     this.loadMeetup();
   }
 
   loadMeetup = () => {
     API.getMeetups()
-      .then(res => this.setState({ meetups: res.data, name: "",link: "", city: "", state: "", who: ""})
+      .then(res => this.setState({ meetups: res.data, name: "", link: "", city: "", state: "", who: "", description: "", })
       )
       .catch(err => console.log(err));
   };
@@ -32,39 +31,42 @@ class Meetup extends Component {
   render() {
     return (
       <Container fluid>
+
+        <Col size="sm-12">
+          <img className="img-fluid rounded" src="https://secure.meetupstatic.com/photos/event/2/e/a/d/highres_450131949.jpeg" alt="">
+          </img>
+        </Col>
+<br>
+</br>
         <Row>
           <Col size="md-12">
-            <Jumbotron>
-              <h1>Meetup</h1>
-            </Jumbotron>
-          </Col>
-          <Col size="md-12">
-            <Jumbotron>
-              <h1>My Saved Meetup Groups</h1>
-            </Jumbotron>
             {this.state.meetups.length ? (
               <List>
                 {this.state.meetups.map(meetup => {
-                   return (
-                  <ListItem key={meetup._id}>
-                    <a href={meetup.link}>
-                      <strong>
-                        {meetup.name} {meetup.city} {meetup.state} by {meetup.who}
-                      </strong>
-                    </a>
-                    <DeleteBtn />
-                  </ListItem>
-                );
-                   })}
+                  return (
+                    <ListItem key={meetup._id}>
+                      <a href={meetup.link}>
+                        <strong>
+                          {meetup.name}
+                        </strong>
+                      </a>
+                      <p>
+                        {meetup.description}
+                        {meetup.city} {meetup.state} by {meetup.who}
+                      </p>
+                      {/* <DeleteBtn /> */}
+                    </ListItem>
+                  );
+                })}
               </List>
             ) : (
-              <h3>No Results to Display</h3>
-            )}
+                <h3>No Results to Display</h3>
+              )}
           </Col>
         </Row>
       </Container>
     );
   }
-} 
+}
 
 export default Meetup;
